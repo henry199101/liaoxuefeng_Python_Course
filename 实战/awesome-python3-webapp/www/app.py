11bb042eff2f38a7cbd1c700d59efee26c354e81
henry199101 <331243543@qq.com>
2017-07-02 17:40:08 +0800

    modify app.py 2nd time

--- 实战/awesome-python3-webapp/www/app.py
+++ 实战/awesome-python3-webapp/www/app.py
@@ -1,25 +1,24 @@
-# coding:utf-8
-
-'''
-编写Web App骨架
-'''
+# app.py
 
 import logging; logging.basicConfig(level=logging.INFO)
-import asyncio
+
+import asyncio, os, json, time
+from datetime import datetime
+
 from aiohttp import web
 
-#制作响应函数
-async def index(request):
-    return web.Response(body=b'<h1>I&apos;m Henry,hello.</h1>',content_type='text/html')
+def index(request):
+	return web.Response(body=b'<h1>Hello,wolrd. I&apos;m Henry.</h1>', content_type='text/html')
 
 
-async def init(loop):#Web app服务器初始化
-    app = web.Application(loop=loop)#制作响应函数集合
-    app.router.add_route(method='GET',path='/',handler=index)#把响应函数添加到响应函数集合
-    srv = await loop.create_server(app.make_handler(),'127.0.0.1',9000)
-    logging.info('server start at http://127.0.0.1:9000')#创建服务器(连接网址、端口，绑定handler)
-    return srv
+@asyncio.coroutine
+def init(loop):
+	app = web.Application(loop=loop)
+	app.router.add_route('GET', '/', index)
+	srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+	logging.info('server started at http://127.0.0.1:9000...')
+	return srv
 
-loop = asyncio.get_event_loop()#创建事件
-loop.run_until_complete(init(loop))#运行
-loop.run_forever()#服务器不关闭
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
