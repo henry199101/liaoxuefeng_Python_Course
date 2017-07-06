b476383514a8868d97eeaa16bf9e0a3c0dc42bfa
henry199101 <331243543@qq.com>
2017-07-06 18:44:01 +0800

    modify orm.py the 2nd time

--- 实战/awesome-python3-webapp/www/orm.py
+++ 实战/awesome-python3-webapp/www/orm.py
@@ -103,5 +103,162 @@ class TextField(Field):
 		super(TextField, self).__init__(name, ddl, primary_key, default)
 # 元类
 class ModelMetaclass(type):
-	def __new__(cls, name, bases, attrs):
-		if name == 'Model':
+	def __new__(cls, name, bases, attrs): # 当前准备创建的类的对象；类的名字；类继承的父类集合；类的方法集合。
+		if name == 'Model': # 排除掉对Model类的修改；
+			return type.__new__(cls, name, bases, attrs)
+		tableName = attrs.get('__table__', None) or name
+		logging.info('found a model: %s (table: %s)'%(name, tableName))
+		# 获取所有的Field和主键名:
+		mappings = dict() # 保存映射关系
+		fields = [] # 保存除主键外的属性
+		primarykey = None
+		for k, v in attrs.items():
+			if isinstance(v, Field):
+				logging.info('Found mapping: %s ==> %s'%(k, v))
+				mappings[k] = v
+				if v.primary_key: # 找到主键名
+					if primarykey:
+						raise StandardError('Duplicate primary key for field: k'%k)
+					primarykey = k # 此列设为列表的主键
+				else:
+					fields.append(k) # 保存除主键外的属性
+		if not primarykey:
+			raise StandardError('primary key not found.')
+		for k in mappings.keys():
+			attrs.pop(k) # 从类属性中删除Field属性,否则，容易造成运行时错误（实例的属性会遮盖类的同名属性）
+		escaped_fields = list(map(lambda f: "`%s`"%f,fields)) # 转换为sql语法
+		# 创建供Model类使用属性
+		attrs['__mappings__'] = mappings # 保存属性和列的映射关系
+		attrs['__table__'] = tableName # 表的名字
+		attrs['__primary_key__'] = primarykey # 主键属性名
+		attrs['__fields__'] = fields # 除主键外的属性名
+		attrs['select'] = 'select `%s`, %s from %s'%s(primarykey, ',',.join(escaped_fields), tableName)
+		attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primarykey, create_args_string(len(escaped_fields) + 1)) # 占位符
+		attrs['__updates__'] = 'update `%s` set %s where `%s`=?'%(tableName, ','.join(map(lambda f: '`%s`=?'%(mappings.get(f).name or f),fields)),primarykey) # 查询列的名字，也看一下在Field定义上有没有定义名字，默认None
+		attrs['__deletes__'] = 'delete from `%s` where `%s`=?'%(tableName, primarykey)
+		return type.__new__(cls, name, bases, attrs)
+
+# 基类Model
+class Model(dict, metaclass = ModelMetaclass):
+	def __init__(self, **kw):
+		super(Model, self).__init__(**kw)
+
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttriuteError('"Model" object has no attribute: %s'%key)
+
+	def __setattr__(self, key, value):
+		self[key] = value
+
+	def getValue(self, key):
+		return getattr(self, key, None) # 直接调回内置函数，注意这里没有下划符,注意这里None的用处,是为了当user没有赋值数据时，返回None，调用于update
+		
+	def getValueOrDefault(self, key):
+		value = getattr(self, key, None) # 第三个参数None，可以在没有返回数值时，返回None，调用于save
+		if not value:
+			field = self.__mappings__[key]
+			if field.default is not None:
+				value = field.default() if callable(field.default) else field.default
+				logging.debug('using default value for %s: %s' % (key, str(value)))
+		return value
+
+	@classmethod
+	@asyncio.coroutine
+	def findall(cls, where=None, args=None, **kw):
+		sql = [cls.__select__]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		if args is None:
+			args = []
+		orderBy = kw.get('orderBy',None)
+		if orderBy:
+			sql.append('order by')
+			sql.append(orderBy)
+		limit = kw.get('limit', None)
+		if limit:
+			sql.append('limit')
+			if isinstance(limit, int):
+				sql.append('?')
+				args.append(limit)
+			elif isinstance(limit, tuple) and len(limit) == 2:
+				sql.append('?, ?')
+				args.extend(limit) # tuple融入list
+			else:
+				raise ValueError('Invalid limit value: %s' %str(limit))
+		rs = await select(' '.join(sql), args)
+		return [cls(**r) for r in rs] # 调试的时候尝试了一下return rs，输出结果一样
+
+	@classmethod
+	@asyncio.coroutine
+	def findnumber(cls, selectField, where=None, args=None):
+		sql = ['select %s __num__ from `%s`' %(selectField, cls.__table__)]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		rs = await select(' '.join(sql), args, 1)
+		if len(rs) == 0:
+			return None
+		return rs[0]['__num__']
+
+	@classmethod
+	@asyncio.coroutine
+	def findnumber(cls, selectField, where=None, args=None):
+		sql = ['select %s __num__ from `%s`' % (selectField, cls.__table__)]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		rs = await select(' '.join(sql), args, 1)
+		if len(rs) == 0:
+			return None
+		return rs[0]['__num__']
+
+	@classmethod
+	@asyncio.coroutine
+	def find(cls, primarykey):
+		sql = '%s where `%s`=?' % (cls.__select__, cls.__primary_key__)
+		rs = await select(sql, [primarykey], 1)
+		if len(rs) == 0:
+			return None
+		return cls(**rs[0])
+
+	@asyncio.coroutine
+	def save(self):
+		args = list(map(self.getValueOrDefault, self.__fields__))
+		args.append(self.getValueOrDefault(self.__primary_key__))
+		rows = await execute(self.__insert__, args)
+		if rows != 1:
+			logging.warn('failed to insert record: affected rows: %s' %rows)
+
+	@asyncio.coroutine
+	def update(self):
+		args = list(map(self.getValue, self.__fields__))
+		args.append(self.getValue(self.__primary_key__))
+		rows = await execute(self.__update__, args)
+		if rows != 1:
+			logging.warn('failed to update by primary key: affected rows: %s' % rows)
+
+	@asyncio.coroutine
+	def remove(self):
+		args = [self.getValue(self.__primary_key__)] # 这里不能使用list()-->'int' object is not iterable
+		rows = await execute(self.__delete__, args)
+		if rows != 1:
+			logging.warn('failed to remove by primary key: affected rows: %s' % rows)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
