b8abedde8c3f0239e140e11a9940300a9e3edc76
henry199101 <331243543@qq.com>
2017-07-08 16:56:08 +0800

    commit app.py

--- 实战/awesome-python3-webapp/www/app.py
+++ 实战/awesome-python3-webapp/www/app.py
@@ -23,3 +23,51 @@ def init(loop): # Web app服务器初始化
 loop = asyncio.get_event_loop() # 创建事件
 loop.run_until_complete(init(loop)) # 运行
 loop.run_forever() # 服务器不关闭
+
+app = web.Application(loop=loop, middlewares=[
+	logger_factory, response_factory
+])
+init_jinja2(app, filters=dict(datetime=datetime_filter))
+add_routes(app, 'handlers')
+add_static(app)
+
+@asyncio.coroutine
+def logger_factory(app, handler):
+	@asyncio.coroutine
+	def logger(request):
+		# 记录日志:
+		logging.info('Request: %s %s' % (request.method, request.path))
+		# 继续处理请求:
+		return (yield from handler(request))
+	return logger
+
+@asyncio.coroutine
+def response_factory(app, handler):
+	@asyncio.coroutine
+	def response(request):
+		# 结果:
+		r = yield from handler(request)
+		if isinstance(r, web.StreamResponse):
+			return r
+		if isinstance(r, bytes):
+			resp = web.Response(body=r)
+			resp.content_type = 'application/octet-stream'
+			return resp
+		if isinstance(r, str):
+			resp = web.Response(body=r.encode('utf-8'))
+			resp.content_type = 'text/html;charset=utf-8'
+			return resp
+		if isinstance(r, dict):
+			...
+
+
+
+
+
+
+
+
+
+
+
+
